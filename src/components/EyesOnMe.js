6c6f58a7835c46a95e6d9ffe31de8e2359635c01
dickson4954 <dicksonmurithi155@gmail.com>
2024-05-02 11:18:59 +0300

    initial commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+  function handleFocus(){
+   console.log("Good!")
+  }
+   function handeleBlur(){
+    console.log("Hey! Eyes on me!")
+   }
+
+
+
+
+
+return(
+    <button onFocus={handleFocus} onBlur={handeleBlur}>
+        Eyes on me
+    </button>
+)
+}
+
+export default EyesOnMe;
